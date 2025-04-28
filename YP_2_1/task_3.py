f4d0a6c48022a7a22bea7675a130342f3e851cba
Vasulio <vas_sab07@mail.ru>
2025-04-28 13:12:51 +0700

    task_3

--- YP_2_1/task_3.py
+++ YP_2_1/task_3.py
@@ -1,1 +1,9 @@
-a
+def meeting(mas):
+	for i in range(len(mas)):
+		for j in range(i+1, len(mas)):
+			if mas[i] == mas[j]:
+				return True
+	return False
+
+nums = list(map(int, input("Введите массив:\n").split()))
+print(meeting(nums))
