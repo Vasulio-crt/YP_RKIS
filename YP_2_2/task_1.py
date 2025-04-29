dd34bfdadcef3181251ed89d7e9759fd99f91eb9
Vasulio <vas_sab07@mail.ru>
2025-04-29 11:11:42 +0700

    added YP_2_2 and made task_1, task_2

---
+++ YP_2_2/task_1.py
@@ -0,0 +1,37 @@
+class Student:
+    def __init__(self, lastName, date, groupNumber, progress: list):
+        self.lastName = lastName
+        self.date = date
+        self.groupNumber = groupNumber
+        if len(progress) != 5:
+            for _ in range(5-len(progress)):
+                progress.append(0)
+            self.progress = progress
+        else:
+            self.progress = progress
+
+    def edit_last_name(self, newLastName):
+        self.lastName = newLastName
+
+    def edit_date(self, newDate):
+        self.date = newDate
+
+    def edit_groupNumber(self, newGroupNumber):
+        self.groupNumber = newGroupNumber
+
+    def print_info(self):
+        print("\nФамилия:", self.lastName)
+        print("Дата рождения:", self.date)
+        print("Группа:", self.groupNumber)
+        print("Оценки:", self.progress)
+
+last_name = input("Введите фамилию: ")
+date = input("Введите дату(DD.MM.YYYY): ")
+group_number = int(input("Введите номер группы: "))
+
+Carol = Student(last_name, date, group_number, [4, 5, 5, 4, 4])
+Carol.print_info()
+Carol.edit_date("20.06.2010")
+Carol.edit_last_name("Mason")
+Carol.edit_groupNumber(632)
+Carol.print_info()
---
+++ YP_2_2/task_2.py
@@ -0,0 +1,36 @@
+# Создайте класс с именем Train, содержащий свойства: название пункта
+# назначения, номер поезда, время отправления. Добавить возможность вывода
+# информации о поезде, номер которого введен пользователем. Написать программу,
+# демонстрирующую все возможности класса;
+class Train:
+	def __init__(self, destination, trainNumber, departure):
+		self.destination = destination
+		self.trainNumber = trainNumber
+		self.departure = departure
+
+	def train_info(self):
+		print("Пункт назначения:", self.destination)
+		print("номер поезда:", self.trainNumber)
+		print("Время отправления:", self.departure, "\n")
+
+def find_train(number, trains):
+	for train in trains:
+		if train.trainNumber == num_train:
+			train.train_info()
+			return True
+	return False
+
+
+trains = [Train("Москва", 315, "08:45"),
+        Train("Томск", 86, "12:30"),
+        Train("Казань", 17, "15:15"),
+        Train("Новосибирск", 54, "20:00")]
+
+for train in trains:
+	train.train_info()
+
+num_train = int(input("Введите номер поезда: "))
+find = find_train(num_train, trains)
+if not find:
+	print("Номер поезда не найден") 
+	
