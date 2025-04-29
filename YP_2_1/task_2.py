3fa9fce74d37948cdaf1e82c6604cb7a8a0878d0
Vasulio <vas_sab07@mail.ru>
2025-04-29 08:57:21 +0700

    edit task2

--- YP_2_1/task_2.py
+++ YP_2_1/task_2.py
@@ -1,23 +1,19 @@
-def combination_sum(candidates, target):
-    candidates.sort()
-    result = []
-    
-    def backtrack(start, mas, target):
-        if target == 0:
-            result.append(mas)
-            return
-        if target < 0:
-            return
-        for i in range(start, len(candidates)):
-            if i > start and candidates[i] == candidates[i - 1]:
-                continue
-            backtrack(i + 1, mas + [candidates[i]], target - candidates[i])
-    
-    backtrack(0, [], target)
-    return result
-
 candidates = list(map(int, input("Введите массив:\n").split()))
 target = int(input("Введите target: "))
-print(combination_sum(candidates, target))
+candidates.sort()
+result = []
 
+def candidates_sum(start, mas, target):
+    if target == 0:
+        result.append(mas)
+        return
+    if target < 0:
+        return
+    for i in range(start, len(candidates)):
+        if i > start and candidates[i] == candidates[i - 1]:
+            continue
+        candidates_sum(i + 1, mas + [candidates[i]], target - candidates[i])
+
+candidates_sum(0, [], target)
+print(result)
 #10 1 2 7 6 1 5  8
